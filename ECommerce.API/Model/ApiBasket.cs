c17d9ccf7dade8e93094f10f8b5c325e1205fe08
unknown <Randy Park>
2020-09-02 09:31:28 -0400

    Added Actor services

---
+++ ECommerce.API/Model/ApiBasket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Model
+{
+    public class ApiBasket
+    {
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("item")]
+        public ApiBasketItem[] Items { get; set; }
+    }
+
+    public class ApiBasketItem
+    {
+        [JsonPropertyName("productId")]
+        public string ProductId { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ ECommerce.API/Model/ApiBasketAddRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Model
+{
+    public class ApiBasketAddRequest
+    {
+        [JsonPropertyName("productId")]
+        public Guid ProductId { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+    }
+}
